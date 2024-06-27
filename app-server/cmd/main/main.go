bc4977fb4649a21df7150ee6b1f2a531b800c7ae
Lukasz Kleszczewski <lukasz.kleszczewski531@gmail.com>
2024-06-27 18:08:59 +0200

    Add simple layout

--- app-server/cmd/main/main.go
+++ app-server/cmd/main/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"app-server/internal/repository"
 	"app-server/internal/utilities"
+	"app-server/internal/ws"
 )
 
 func main() {
 	database := repository.InitDB()
 	// repository.PerformMigration(database)
+	clientManager := ws.NewClientManager()
 
-	r := utilities.ServerRouter(database.GetDB())
+	r := utilities.ServerRouter(database.GetDB(), clientManager)
 	r.Run()
 }
--- app-server/internal/utilities/router.go
+++ app-server/internal/utilities/router.go
@@ -3,13 +3,16 @@ package utilities
 import (
 	"app-server/internal/auth"
 	"app-server/internal/mock"
+	"app-server/internal/room"
+	"app-server/internal/ws"
 	"database/sql"
 
 	"github.com/gin-gonic/gin"
 )
 
-func ServerRouter(database *sql.DB) *gin.Engine {
+func ServerRouter(database *sql.DB, clientManager *ws.ClientManager) *gin.Engine {
 	r := gin.Default()
+	r.RedirectTrailingSlash = true
 	secret_byte := getSecretKey()
 	addLogger(r)
 
@@ -22,5 +25,8 @@ func ServerRouter(database *sql.DB) *gin.Engine {
 	// Endpoints with JWT middleware
 	auth_endpoints := r.Group("/api", auth.AuthMiddleware(secret_byte))
 	auth_endpoints.GET("/token_check", mock.TokenCheckHandler)
+
+	room.RegisterRoutes(r, clientManager)
+
 	return r
 }
